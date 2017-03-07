be8cfbfdf30c854ad616a27930e6e6599edf0f56
ljpi <lesterpi92@gmail.com>
2017-03-06 22:47:43 -0800

    add in restaurant industry data

--- yelp.R
+++
@@ -1,9 +0,0 @@
-library(jsonlite)
-
-setwd("/Users/lesterpi/Desktop/Folders/Random Junk/other/yelp_dataset_challenge_round9")
-
-#load data example
-json_file = file("yelp_academic_dataset_checkin.json")
-json_data = jsonlite::stream_in(json_file)
-head(json_data)
-length(json_data$business_id)
